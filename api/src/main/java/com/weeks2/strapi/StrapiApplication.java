6bab38d5f47400325c14b20fa06830b28530bbcb
Christian Ibarra <chris.cjarne@gmail.com>
2023-12-19 21:48:17 -0600

    Update StrapiApplication.java

--- api/src/main/java/com/weeks2/strapi/StrapiApplication.java
+++ api/src/main/java/com/weeks2/strapi/StrapiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StrapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StrapiApplication.class, args);
 	}
